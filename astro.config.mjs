0ac6f787967693322a70190d12448bdde0aeb2be
Maxim Igenbergs <migenbergs@gmail.com>
2026-03-12 11:10:31 +0100

    neue domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://villa-bauseweinallee.de",
+  site: "https://villa-bausewein.de",
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
